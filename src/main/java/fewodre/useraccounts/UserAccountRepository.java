579f5b00dc90f275c1d80e692ea999ceceff2e69
Beanie <janu2010@hotmail.de>
2020-11-17 14:37:22 +0100

    added some methods to UserAccountRepository interface.

--- src/main/java/fewodre/useraccounts/UserAccountRepository.java
+++ src/main/java/fewodre/useraccounts/UserAccountRepository.java
@@ -1,6 +1,13 @@
 package fewodre.useraccounts;
 
+import org.h2.engine.User;
+import org.salespointframework.useraccount.UserAccount;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserAccountRepository extends CrudRepository<UserAccountEntity, Long> {
+
+	UserAccountEntity findByEmail(String email);
+	UserAccountEntity findByUuid(String uuid);
+	UserAccountEntity findById(String id);
+
 }
