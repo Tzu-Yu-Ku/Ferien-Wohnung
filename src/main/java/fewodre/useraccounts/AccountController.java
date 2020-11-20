6ff101671f97423a3a28de91d43ec207cfd36a2f
Beanie <janu2010@hotmail.de>
2020-11-20 18:44:39 +0100

    added validation messages resource bundle. created first (kind of) working validation for our register form and created a fitting post-mapping in AccountController which already receives almost all validation errors that can occur when entering wrong information in the register form. not even close to being finished yet.

--- src/main/java/fewodre/useraccounts/AccountController.java
+++ src/main/java/fewodre/useraccounts/AccountController.java
@@ -1,16 +1,24 @@
 package fewodre.useraccounts;
 
+import org.h2.engine.Mode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
 
 	private final AccountManagement accountManagement;
+	private static final Logger LOG = LoggerFactory.getLogger(AccountController.class);
 
 	AccountController(AccountManagement accountManagement) {
 		Assert.notNull(accountManagement, "CustomerManagement must not be null!");
@@ -24,7 +32,14 @@ public class AccountController {
 	}
 
 	@PostMapping("/register")
-	public String registerNewAccount() {
+	public String registerNewAccount(@Valid @ModelAttribute("registrationForm") RegistrationForm registrationForm,
+	                                 BindingResult result, Model model) {
+		LOG.info(registrationForm.getBirthDate());
+		if (result.hasErrors()) {
+			LOG.info(result.getAllErrors().toString());
+			return "register";
+		}
+
 		return "redirect:/login";
 	}
 
--- src/main/java/fewodre/useraccounts/RegistrationForm.java
+++ src/main/java/fewodre/useraccounts/RegistrationForm.java
@@ -1,32 +1,63 @@
 package fewodre.useraccounts;
 
-import javax.validation.constraints.AssertTrue;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.*;
+import java.util.Date;
 
 class RegistrationForm {
 
-	@NotBlank(message = "{RegistrationForm.username.NotEmpty}")
+	@NotBlank(message = "{RegistrationForm.firstname.NotEmpty}")
+	@NotEmpty(message = "{RegistrationForm.firstname.NotEmpty}")
 	private final String firstName;
 
-	@NotBlank(message = "{RegistrationForm.username.NotEmpty}")
+	@NotBlank(message = "{RegistrationForm.lastname.NotEmpty}")
 	private final String lastName;
+
+	@NotBlank(message = "{RegistrationForm.email.NotEmpty}")
+	@Email(message = "{RegistrationForm.email.NotValid}")
+	private final String email;
 
 	@NotBlank(message = "{RegistrationForm.password.NotEmpty}")
 	private final String password;
 
-	@NotBlank(message = "{RegistrationForm.address.NotEmpty}")
-	@Email(message = "{RegistrationForm.address.NotValid}")
-	private final String email;
+	@NotBlank(message = "{RegistrationForm.password_confirm.NotEmpty}")
+	private final String passwordConfirm;
 
-	@AssertTrue(message = "{RegistrationForm.terms.notAgreed}")
+	@NotBlank(message = "{RegistrationForm.password_confirm.NotEmpty}")
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	private final String birthDate;
+
+	@NotBlank(message = "{RegistrationForm.street.NotEmpty}")
+	private final String street;
+
+	@NotBlank(message = "{RegistrationForm.housenumber.NotEmpty}")
+	private final String houseNumber;
+
+	@NotBlank(message = "{RegistrationForm.postcode.NotEmpty}")
+	@Size(min=5, max=5, message = "PLZ muss fünfstellig sein.")
+	private final String postcode;
+
+	@NotBlank(message = "{RegistrationForm.city.NotEmpty}")
+	private final String city;
+
+	@AssertTrue(message = "{RegistrationForm.terms.NotAgreed}")
 	private final Boolean terms;
 
-	public RegistrationForm(String firstName, String lastName, String password, String email, Boolean terms) {
+	public RegistrationForm(String firstName, String lastName, String email, String password, String passwordConfirm,
+	                        String birthDate, String street, String houseNumber, String postcode, String city, Boolean terms) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.password = password;
 		this.email = email;
+		this.password = password;
+		this.passwordConfirm = passwordConfirm;
+		this.birthDate = birthDate;
+		this.street = street;
+		this.houseNumber = houseNumber;
+		this.postcode = postcode;
+		this.city = city;
 		this.terms = terms;
 	}
 
@@ -34,16 +65,43 @@ class RegistrationForm {
 		return firstName;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getLastName() {
+		return lastName;
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
 	public Boolean getTerms() {
 		return terms;
 	}
-
 }
