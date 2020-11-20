b836eae62289bf495455e3fdb66588230ecf3127
Beanie <janu2010@hotmail.de>
2020-11-20 16:46:12 +0100

    fixed navbar and footer inclusion on register and login page. started implementing AccountController to enable first basic registration of new users.

--- src/main/java/fewodre/useraccounts/AccountController.java
+++ src/main/java/fewodre/useraccounts/AccountController.java
@@ -1,15 +1,31 @@
 package fewodre.useraccounts;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AccountController {
 
-	@GetMapping("/account")
-	public String account() {
-		return "account";
+	private final AccountManagement accountManagement;
+
+	AccountController(AccountManagement accountManagement) {
+		Assert.notNull(accountManagement, "CustomerManagement must not be null!");
+		this.accountManagement = accountManagement;
 	}
 
+	@GetMapping("/register")
+	public String register(Model model, RegistrationForm registrationForm) {
+		model.addAttribute("registrationForm", registrationForm);
+		return "register";
+	}
+
+	@PostMapping("/register")
+	public String registerNewAccount() {
+		return "redirect:/login";
+	}
 
 }
