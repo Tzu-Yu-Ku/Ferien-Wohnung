52229ec640399541dcab4f815fa319588c732bbf
Gloria <bearsister2002@gmail.com>
2020-11-21 01:04:45 +0100

    housedetails add dates

--- src/main/java/fewodre/bookings/BookingRepository.java
+++ src/main/java/fewodre/bookings/BookingRepository.java
@@ -1,8 +1,15 @@
 package fewodre.bookings;
 
+import fewodre.useraccounts.AccountEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookingRepository extends CrudRepository<BookingEntity, Long> {
+
+	Iterable<BookingEntity> findBookingsByHost(String host){};
+
+	Iterable<BookingEntity> findBookingsBy
+
+
 
 
 
