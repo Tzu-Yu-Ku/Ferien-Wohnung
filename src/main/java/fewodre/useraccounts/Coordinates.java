7883eb0eee87d636e32fd71a920248524dabb61e
jukolein <julian.boehm@gmx.de>
2020-11-23 10:05:17 +0100

    register new hosts, redirect after login improved, first map-code

---
+++ src/main/java/fewodre/useraccounts/Coordinates.java
@@ -0,0 +1,17 @@
+package fewodre.useraccounts;
+import com.mysema.commons.lang.Pair;
+
+import java.awt.*;
+
+
+public class Coordinates {
+	public String size;
+//	static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+//	static double width = screenSize.getWidth();
+//	static double height = screenSize.getHeight();
+	public Coordinates(String size){
+		System.out.println(size);
+	}
+
+}
+
