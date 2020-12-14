cb6e879af6dda65dccf60ea68d8bff61f04a8c37
Beanie <janu2010@hotmail.de>
2020-12-14 16:06:01 +0100

    successfully populate all account types on /manageaccounts. ability to enable new tenant accounts on this page aswell.

--- src/main/java/fewodre/useraccounts/AccountDataInitializer.java
+++ src/main/java/fewodre/useraccounts/AccountDataInitializer.java
@@ -41,15 +41,15 @@ public class AccountDataInitializer implements DataInitializer {
 		TenantRegistrationForm tenantRegistrationForm = new TenantRegistrationForm("Janujan", "Jan",
 				"tenant@tenant", "123", "123", "1900-01-01", "Test Street",
 				"1", "12345", "Dresden", true);
-//		HostRegistrationForm hostRegistrationForm = new HostRegistrationForm("Janujan", "Jan",
-//				"host@host", "123", "123", "1900-01-01", "Test Street",
-//				"1", "12345", "Dresden", "DE55500105171938297534", "MALADE51AKI");
-//		EventEmployeeRegistrationForm eventEmployeeRegistrationForm = new EventEmployeeRegistrationForm("Janujan", "Jan",
-//				"event@employee", "123", "123", "EventBois Dresden GmbH");
+		HostRegistrationForm hostRegistrationForm = new HostRegistrationForm("Janujan", "Jan",
+				"host@host", "123", "123", "1900-01-01", "Test Street",
+				"1", "12345", "Dresden", "DE55500105171938297534", "MALADE51AKI");
+		EventEmployeeRegistrationForm eventEmployeeRegistrationForm = new EventEmployeeRegistrationForm("Janujan", "Jan",
+				"event@employee", "123", "123", "EventBois Dresden GmbH");
 
 		accountManagement.createTenantAccount(tenantRegistrationForm);
-//		accountManagement.createHostAccount(hostRegistrationForm);
-//		accountManagement.createEventEmployeeAccount(eventEmployeeRegistrationForm);
+		accountManagement.createHostAccount(hostRegistrationForm);
+		accountManagement.createEventEmployeeAccount(eventEmployeeRegistrationForm);
 
 		userAccountManagement.create("admin", Password.UnencryptedPassword.of("admin"), ADMIN_ROLE);
 		userAccountManagement.create("host", Password.UnencryptedPassword.of("host"), HOST_ROLE);
