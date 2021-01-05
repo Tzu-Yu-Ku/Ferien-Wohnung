7d1cee9bbbffbd8d1cbb8c696b788d83fe8d022b
MartenFree <martenfreese@web.de>
2021-01-05 16:17:56 +0100

    Änderungen und verbesserungen an HH löschen und bearbeiten

--- src/main/java/fewodre/catalog/holidayhomes/HolidayHomeForm.java
+++ src/main/java/fewodre/catalog/holidayhomes/HolidayHomeForm.java
@@ -104,14 +104,4 @@ public class HolidayHomeForm {
 				getCapacity(), Money.of(getPrice(), "EUR"));
 	}
 
-	public void editHolidayHome(HolidayHome holidayHome) {
-		holidayHome.setName(getName());
-		holidayHome.setHostMail(getHostMail());
-		holidayHome.setDescription(getDescription());
-		//image
-		holidayHome.setPlace(new Place(getStreet(), getNumber(), getPostalnumber(), getCity(), 1, 1));
-		holidayHome.setIsBookable(true);
-		holidayHome.setCapacity(getCapacity());
-		holidayHome.setPrice(Money.of(getPrice(), "EUR"));
-	}
 }
