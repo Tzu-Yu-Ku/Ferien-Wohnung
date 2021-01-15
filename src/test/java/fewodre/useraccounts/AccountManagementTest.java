ec30a50476a9409dae045267746ff9cc2fb7714e
Beanie <janu2010@hotmail.de>
2021-01-15 17:28:51 +0100

    added UnitTest for createTenantAccount method in AccountManagement

--- src/test/java/fewodre/useraccounts/AccountManagementTest.java
+++ src/test/java/fewodre/useraccounts/AccountManagementTest.java
@@ -2,8 +2,13 @@ package fewodre.useraccounts;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.salespointframework.useraccount.Password;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManagement;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 
 class AccountManagementTest {
 
@@ -13,6 +18,28 @@ class AccountManagementTest {
 
 	@Test
 	void createTenantAccount() {
+
+		AccountRepository accountRepository = mock(AccountRepository.class);
+		when(accountRepository.save(any())).then(i -> i.getArgument(0));
+
+		UserAccountManagement userAccountManager = mock(UserAccountManagement.class);
+		UserAccount userAccount = mock(UserAccount.class);
+		when(userAccountManager.create(any(), any(), any(), any())).thenReturn(userAccount);
+
+		AccountManagement accountManagement = new AccountManagement(accountRepository, userAccountManager);
+
+		TenantRegistrationForm form = new TenantRegistrationForm("Firstname", "Lastname",
+				"e@mail.com", "123", "123", "1999-01-01", "Street",
+				"1", "12345", "City", true);
+		AccountEntity tenantAccountEnitity = accountManagement.createTenantAccount(form);
+
+		verify(userAccountManager, times(1)) //
+				.create(eq(form.getEmail()), //
+						eq(Password.UnencryptedPassword.of(form.getPassword())), //
+						eq(form.getEmail()), //
+						eq(AccountManagement.UNACTIVATED_TENANT_ROLE));
+
+		assertThat(tenantAccountEnitity.getAccount()).isNotNull();
 	}
 
 	@Test
