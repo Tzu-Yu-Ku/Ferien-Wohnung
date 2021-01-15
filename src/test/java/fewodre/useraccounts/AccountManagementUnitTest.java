9c942ea74f797c0a18cb20f5a30474ac45b96934
Beanie <janu2010@hotmail.de>
2021-01-15 18:49:02 +0100

    added some tests for AccountManagement.

--- src/test/java/fewodre/useraccounts/AccountManagementUnitTest.java
+++ src/test/java/fewodre/useraccounts/AccountManagementUnitTest.java
@@ -1,5 +1,6 @@
 package fewodre.useraccounts;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.salespointframework.useraccount.Password;
 import org.salespointframework.useraccount.UserAccount;
@@ -11,25 +12,31 @@ import static org.mockito.Mockito.*;
 
 class AccountManagementUnitTest {
 
+	AccountManagement accountManagement;
+	UserAccountManagement userAccountManagement;
+	AccountRepository accountRepository;
+
+	@BeforeEach
+	void setUp() {
+		accountRepository = mock(AccountRepository.class);
+		when(accountRepository.save(any())).then(i -> i.getArgument(0));
+
+		userAccountManagement = mock(UserAccountManagement.class);
+		UserAccount userAccount = mock(UserAccount.class);
+		when(userAccountManagement.create(any(), any(), any(), any())).thenReturn(userAccount);
+
+		accountManagement = new AccountManagement(accountRepository, userAccountManagement);
+	}
 
 	@Test
 	public void createTenantAccount() {
-
-		AccountRepository accountRepository = mock(AccountRepository.class);
-		when(accountRepository.save(any())).then(i -> i.getArgument(0));
-
-		UserAccountManagement userAccountManager = mock(UserAccountManagement.class);
-		UserAccount userAccount = mock(UserAccount.class);
-		when(userAccountManager.create(any(), any(), any(), any())).thenReturn(userAccount);
-
-		AccountManagement accountManagement = new AccountManagement(accountRepository, userAccountManager);
 
 		TenantRegistrationForm form = new TenantRegistrationForm("Firstname", "Lastname",
 				"e@mail.com", "123", "123", "1999-01-01", "Street",
 				"1", "12345", "City", true);
 		AccountEntity tenantAccountEnitity = accountManagement.createTenantAccount(form);
 
-		verify(userAccountManager, times(1)) //
+		verify(userAccountManagement, times(1)) //
 				.create(eq(form.getEmail()), //
 						eq(Password.UnencryptedPassword.of(form.getPassword())), //
 						eq(form.getEmail()), //
@@ -40,10 +47,35 @@ class AccountManagementUnitTest {
 
 	@Test
 	void createHostAccount() {
+
+		HostRegistrationForm form = new HostRegistrationForm("Firstname", "Lastname",
+				"e@mail.com", "123", "123", "1999-01-01", "Street",
+				"1", "12345", "City", "DE123123123123", "MALADE61AKA");
+		AccountEntity tenantAccountEnitity = accountManagement.createHostAccount(form);
+
+		verify(userAccountManagement, times(1)) //
+				.create(eq(form.getEmail()), //
+						eq(Password.UnencryptedPassword.of(form.getPassword())), //
+						eq(form.getEmail()), //
+						eq(AccountManagement.HOST_ROLE));
+
+		assertThat(tenantAccountEnitity.getAccount()).isNotNull();
 	}
 
 	@Test
 	void createEventEmployeeAccount() {
+
+		EventEmployeeRegistrationForm form = new EventEmployeeRegistrationForm("Firstname", "Lastname",
+				"e@mail.com", "123", "123", "Company");
+		AccountEntity tenantAccountEnitity = accountManagement.createEventEmployeeAccount(form);
+
+		verify(userAccountManagement, times(1)) //
+				.create(eq(form.getEmail()), //
+						eq(Password.UnencryptedPassword.of(form.getPassword())), //
+						eq(form.getEmail()), //
+						eq(AccountManagement.EVENTEMPLOYEE_ROLE));
+
+		assertThat(tenantAccountEnitity.getAccount()).isNotNull();
 	}
 
 	@Test
@@ -68,5 +100,6 @@ class AccountManagementUnitTest {
 
 	@Test
 	void getRepository() {
+		assertThat(accountManagement.getRepository()).isEqualTo(accountRepository);
 	}
 }
