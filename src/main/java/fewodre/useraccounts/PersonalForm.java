47e6e508b617e726929a234659f398e1d465706d
Beanie <janu2010@hotmail.de>
2021-01-15 17:07:36 +0100

    removed some duplicate code in more Form classes.

---
+++ src/main/java/fewodre/useraccounts/PersonalForm.java
@@ -0,0 +1,56 @@
+package fewodre.useraccounts;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class PersonalForm extends RegistrationForm {
+
+	@NotBlank(message = "{RegistrationForm.password_confirm.NotEmpty}")
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	private final String birthDate;
+
+	@NotBlank(message = "{RegistrationForm.street.NotEmpty}")
+	private final String street;
+
+	@NotBlank(message = "{RegistrationForm.housenumber.NotEmpty}")
+	private final String houseNumber;
+
+	@NotBlank(message = "{RegistrationForm.postcode.NotEmpty}")
+	@Size(min = 5, max = 5, message = "PLZ muss fünfstellig sein.")
+	private final String postcode;
+
+	@NotBlank(message = "{RegistrationForm.city.NotEmpty}")
+	private final String city;
+
+	public PersonalForm(String firstName, String lastName, String email, String password, String passwordConfirm,
+	                    String birthDate, String street, String houseNumber, String postcode, String city) {
+		super(firstName, lastName, email, password, passwordConfirm);
+		this.birthDate = birthDate;
+		this.street = street;
+		this.houseNumber = houseNumber;
+		this.postcode = postcode;
+		this.city = city;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+}
--- src/main/java/fewodre/useraccounts/TenantRegistrationForm.java
+++ src/main/java/fewodre/useraccounts/TenantRegistrationForm.java
@@ -1,63 +1,17 @@
 package fewodre.useraccounts;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.validation.constraints.AssertTrue;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
-class TenantRegistrationForm extends RegistrationForm {
-
-	@NotBlank(message = "{RegistrationForm.password_confirm.NotEmpty}")
-	@DateTimeFormat(pattern = "yyyy-mm-dd")
-	private final String birthDate;
-
-	@NotBlank(message = "{RegistrationForm.street.NotEmpty}")
-	private final String street;
-
-	@NotBlank(message = "{RegistrationForm.housenumber.NotEmpty}")
-	private final String houseNumber;
-
-	@NotBlank(message = "{RegistrationForm.postcode.NotEmpty}")
-	@Size(min = 5, max = 5, message = "PLZ muss fünfstellig sein.")
-	private final String postcode;
-
-	@NotBlank(message = "{RegistrationForm.city.NotEmpty}")
-	private final String city;
+class TenantRegistrationForm extends PersonalForm {
 
 	@AssertTrue(message = "{RegistrationForm.terms.NotAgreed}")
 	private final Boolean terms;
 
-	public TenantRegistrationForm(String firstName, String lastName, String email, String password, String passwordConfirm,
-	                              String birthDate, String street, String houseNumber, String postcode, String city, Boolean terms) {
-		super(firstName, lastName, email, password, passwordConfirm);
-		this.birthDate = birthDate;
-		this.street = street;
-		this.houseNumber = houseNumber;
-		this.postcode = postcode;
-		this.city = city;
+	public TenantRegistrationForm(String firstName, String lastName, String email, String password,
+	                              String passwordConfirm, String birthDate, String street, String houseNumber,
+	                              String postcode, String city, Boolean terms) {
+		super(firstName, lastName, email, password, passwordConfirm, birthDate, street, houseNumber, postcode, city);
 		this.terms = terms;
-	}
-
-	public String getBirthDate() {
-		return birthDate;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-
-	public String getHouseNumber() {
-		return houseNumber;
-	}
-
-	public String getPostcode() {
-		return postcode;
-	}
-
-	public String getCity() {
-		return city;
 	}
 
 	public Boolean getTerms() {
